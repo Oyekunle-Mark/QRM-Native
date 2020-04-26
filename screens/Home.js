a59ebd422fbe794bc6b989ff879da9a08fa35ac8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-26 17:01:22 +0100

    Renders a list of cards

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useCallback} from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, FlatList, StyleSheet} from 'react-native';
+import {Card, ListItem, Button, Icon} from 'react-native-elements';
 
 const URL = 'http://localhost:3000/api/v1/codes';
 
@@ -20,17 +21,39 @@ const Home = () => {
   }, []);
 
   return (
-    <View>
-      <Image
-        style={{width: 50, height: 50}}
-        source={{
-          uri:
-            'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHQAAAB0CAYAAABUmhYnAAAAAklEQVR4AewaftIAAAKjSURBVO3BQW7gSAwEwSxC//9yro88NSBI8noIRsQfrDGKNUqxRinWKMUapVijFGuUYo1SrFGKNUqxRinWKMUapVijFGuUYo1y8VASfpPKSRI6lS4JncpJEn6TyhPFGqVYoxRrlIuXqbwpCXeonKg8ofKmJLypWKMUa5RijXLxsSTcoXJHEk5UuiR0Kk8k4Q6VLxVrlGKNUqxRLoZROVHpktCp/MuKNUqxRinWKBfDJOFEZbJijVKsUYo1ysXHVP6SJHQqT6j8JcUapVijFGuUi5cl4f+k0iWhU+mS0KmcJOEvK9YoxRqlWKPEH/zDknCHymTFGqVYoxRrlPiDB5LQqXRJeJPKSRLuUDlJwptUvlSsUYo1SrFGufiYypeS0Kl0SThJQqfSqZwk4UTlJAmdyhPFGqVYoxRrlIuHVE6ScIdKl4RO5SQJncpJErok3KHSJeEkCZ3Km4o1SrFGKdYoFw8l4QmVE5U7VLok3KFyRxJOktCpdEnoVJ4o1ijFGqVYo1z8MpWTJHxJpUtCl4Q7VO5IwpeKNUqxRinWKPEH/7AknKicJKFTuSMJncr/qVijFGuUYo1y8VASfpNKp3KShBOVLgmdSpeETqVLQqfSJaFTeVOxRinWKMUa5eJlKm9KwkkSOpVO5Q6VNyXhJAmdyhPFGqVYoxRrlIuPJeEOlTcl4USlS8ITKnck4U3FGqVYoxRrlIthktCpnCShUzlJQpeETuUOlTcVa5RijVKsUS7WIypdEk5UvlSsUYo1SrFGufiYypdUTpJwRxI6lU7lLyvWKMUapVijXLwsCb8pCZ1Kp/JEEk5UOpWTJHQqbyrWKMUapVijxB+sMYo1SrFGKdYoxRqlWKMUa5RijVKsUYo1SrFGKdYoxRqlWKMUa5RijfIfpYcH5aVnI3cAAAAASUVORK5CYII=',
-        }}
+    <View style={styles.container}>
+      <FlatList
+        data={qRCodes}
+        renderItem={({item}) => (
+          <Card title={item.title} image={item.code}>
+            <Text style={styles.text}>
+              The idea with React Native Elements is more about component
+              structure than actual design.
+            </Text>
+            <Button
+              icon={<Icon name="code" color="#ffffff" />}
+              buttonStyle={styles.button}
+              title="VIEW NOW"
+            />
+          </Card>
+        )}
       />
-      <Text>Home Screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  button: {
+    borderRadius: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+  },
+  text: {marginBottom: 10},
+});
 
 export default Home;
