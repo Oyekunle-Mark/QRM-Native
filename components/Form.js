4b284d709f3bf491ce40922d0991192bc089ada0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-26 19:25:57 +0100

    Export the form component

--- components/Form.js
+++ components/Form.js
@@ -31,3 +31,41 @@ const CodeForm = () => {
       </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      paddingTop: 20,
+    },
+    button: {
+      borderRadius: 5,
+      marginLeft: 0,
+      marginRight: 0,
+      marginBottom: 0,
+    },
+    image: {
+      width: 300,
+      height: 300,
+    },
+    cardContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+    },
+    input: {
+      margin: 10,
+    },
+    submit: {
+      marginHorizontal: 20,
+      marginBottom: 30,
+    },
+    icon: {
+      marginHorizontal: 10,
+    },
+    emptyTextMessage: {
+      textAlign: 'center',
+      fontSize: 18,
+    },
+  });
+
+export default CodeForm;
